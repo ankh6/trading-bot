592d9cf9924d39d3dfca4bac414f7741f558f37f
ankh6 <andynzemokalumu@live.be>
2022-06-20 09:27:39 +0200

    Updated the helper function fetch_exchange_given_trading_pairs() to be less restrictive Added docstring for all methods Added function that retrieves the trading attributes from the symbol on the exchange Added function that creates a dataframe given an array of trading attributes. This functions drops a useless feature/column provided by Binance API

--- exchanges/Market.py
+++ exchanges/Market.py
@@ -2,6 +2,8 @@ from abc import ABCMeta
 from typing import List, Tuple
 from Constants import BASE_URL, API_VERSION
 from requests import get
+import numpy as np
+import pandas as pd
 
 
 class Exchange(metaclass=ABCMeta):
@@ -14,6 +16,16 @@ class Exchange(metaclass=ABCMeta):
         self._rate_limit : int = None
 
     def fetch_exchange_given_trading_pairs(self, url: str, version: str, endpoint: str, params: List[Tuple[str,str]]):
+        ''' Helper function to fetch data given a url, endpoint and request parameters (optional)
+
+        Arguments:
+        url: string, the base URI (Uniform Resource Identifier)
+        endpoint: a string, the endpoint
+        params: list of tuples, the request parameters (optional)
+        
+        Return:
+        response, the resources in a json format
+        '''
         request_parameters = params
         full_path = url+ version + "/" + endpoint
         response = get(url=full_path, params=request_parameters)
@@ -21,6 +33,9 @@ class Exchange(metaclass=ABCMeta):
     
     def initialize(self, trading_pair : str):
         ''' Initializes the Exchange object with its properties
+
+        Arguments:
+        trading_pair: A string without a "/" separating the base asset from the quote, e.g. ETH/USDC -> ETHUDSC
         '''
         exchange_info = self.fetch_exchange_given_trading_pairs(url=BASE_URL, version=API_VERSION, endpoint="exchangeInfo" ,params=[("symbol", trading_pair)])
         rate_limit = exchange_info["rateLimits"]    
@@ -31,12 +46,32 @@ class Exchange(metaclass=ABCMeta):
         self._symbols = symbols[0]["symbol"]
         self.base_asset = symbols[0]["baseAsset"]
         self.quote_asset = symbols[0]["quoteAsset"]
+    
+    
+    def create_symbol_dataframe(self, symbol_trading_attributes: np.array):
+        ''' Generates a DataFrame that stores attributes of a the trading pair
+
+        Arguments:
+        symbol_trading_attributes: a list of attributes that are available on the exchange for the given pair
+        '''
+        df = pd.DataFrame(data=symbol_trading_attributes, columns=["Open time", "Open", "High", "Low", "Close", "Volume", "Close time", "Quote asset volumes", "Number of trades", "Taker buy base volume", "Take buy quote volume", "Ignore"])
+        # Binance stores a value at the last index of each set of observations
+        # Per documentation, this value is set as "Ignored"
+        # We drop the column that stores these values
+        df.drop(labels=["Ignore"], axis = 1, inplace=True) 
 
     def fetch_price_data_given_symbol(self, time: str, interval_type:str):
+        ''' Fetches the symbol trading attributes from Binance API
+
+        Arguments:
+        time: an integer from 1 to 9
+        interval_type: case-sensitive, s(SECOND), m(MINUTE), h(HOUR), d(DAY)
+        See their documentation for the enums they support: https://binance-docs.github.io/apidocs/spot/en/#limits
+
+        Returns:
+        exchange_info: a numpy array that stores the trading attributes
+        '''
         _interval = time + interval_type
         exchange_info = self.fetch_exchange_given_trading_pairs(url=BASE_URL, version=API_VERSION, endpoint="klines",params=[("symbol", self.symbol),("interval", _interval)])
-        # TO-DO
-        # No need to define types of interval because data will be fetched once the script runs (e.g. every 5 minute)
-        # Convert binance array to numpy array
-        # Create pandas dataframe for data manipulations
-        # Name of columns depend on data we fetch
+        return np.array(exchange_info,copy=False)
+        
