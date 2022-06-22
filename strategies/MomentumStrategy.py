ee2c9aa703fe791229f7a472634de2361de9c833
ankh6 <andynzemokalumu@live.be>
2022-06-23 00:31:27 +0200

    Added documentation

--- strategies/MomentumStrategy.py
+++ strategies/MomentumStrategy.py
@@ -11,8 +11,8 @@ def compute_short_long_ema(trading_attributes_dataframe: pd.DataFrame) -> Tuple[
     trading_attributes_dataframe: the source dataframe that stores the closing prices of the base asset
 
     Returns:
-    latest_short_term_ema: The short-term EMA for the last observation givne the short timeperiod, i.e. 12 observations
-    latest_long_term_ema: The long-term EMA for the last observation given the long timeperiod, i.e. 26 observations
+    ema_short_term: The short-term EMA pandas Series
+    ema_long_term: The long-term EMA pandas Series
     '''
     close_prices_series = trading_attributes_dataframe.loc[:, "Close"]
     ema_short_term = EMA(close_prices_series, timeperiod=EMA_SHORT_TERM)
@@ -21,19 +21,40 @@ def compute_short_long_ema(trading_attributes_dataframe: pd.DataFrame) -> Tuple[
 
 
 def find_latest_short_long_ema(trading_attributes_dataframe: pd.DataFrame):
+    ''' Computes the EMA values for short and long-term periods
+
+    Arguments:
+    trading_attributes_dataframe: the source dataframe that stores the closing prices of the base asset
+
+    Returns:
+    latest_short_term_ema: The short-term EMA for the last observation givne the short timeperiod, i.e. 12 observations
+    latest_long_term_ema: The long-term EMA for the last observation given the long timeperiod, i.e. 26 observations
+
+    '''
     ema_short_term, ema_long_term = compute_short_long_ema(trading_attributes_dataframe)
     latest_short_term_ema  = ema_short_term.loc[ema_short_term.size - 1]
     latest_long_term_ema = ema_long_term.loc[ema_long_term.size - 1]
     return (latest_short_term_ema, latest_long_term_ema)
 
-def find_trend_direction(trading_attributes_dataframe: pd.DataFrame) -> bool:
+def find_trend_direction(trading_attributes_dataframe: pd.DataFrame):
+    ''' Helper method to find the direction of the trend
+
+    This functions relies on calculation the pandas Series that is the difference between the short and long-term EMA
+
+    Arguments:
+    trading_attributes_dataframe: the source dataframe that stores the closing prices of the base asset
+    
+    Returns:
+    An signed integer that is the difference between the short and long-term EMA at the last observation
+
+    '''
     ema_short_term, ema_long_term = compute_short_long_ema(trading_attributes_dataframe)
     # Finds cross, i.e. a point where both EMAs have the same value
     res = ema_short_term - ema_long_term
     return res.loc[res.size - 1]
 
 def compute_rsi(trading_attributes_dataframe: pd.DataFrame) -> int :
-    ''' Computes the Exponential Moving Average on the close prices
+    ''' Computes the Relative Strength Index on the close prices
     Arguments:
     trading_attributes_dataframe: the source dataframe that stores the closing prices of the base asset
 
